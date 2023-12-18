b27b7c9046bb1d60d291c1ce42934470567b0e16
Yusuf Bin Musa <yusuf.musa3@gmail.com>
2023-12-18 12:28:22 +0800

    fix: ci binaries

--- ci.sh
+++ ci.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+OS=$(uname -s | tr '[:upper:]' '[:lower:]')
+ARCH=$(uname -m | tr '[:upper:]' '[:lower:]')
+
+BIN=".github/scripts/dist/yacar_ci_${OS}_${ARCH}"
+
+echo $BIN
+
 ROOT_DIR=$(git rev-parse --show-toplevel)
-cd $ROOT_DIR/.github/scripts
-ROOT_DIR=$ROOT_DIR go run cmd/yacar_ci.go
+ROOT_DIR=${ROOT_DIR} ${BIN}
