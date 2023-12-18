4f624c19e56ff4fd6a60f402caef5526c97d0a75
Yusuf Bin Musa <yusuf.musa3@gmail.com>
2023-12-18 13:11:54 +0800

    test: ci

--- ci.sh
+++ ci.sh
@@ -3,6 +3,10 @@
 OS=$(uname -s | tr '[:upper:]' '[:lower:]')
 ARCH=$(uname -m | tr '[:upper:]' '[:lower:]')
 
+if [ "${ARCH}" == "x86_64" ]; then
+    ARCH="amd64"
+fi
+
 BIN=".github/scripts/dist/yacar_ci_${OS}_${ARCH}"
 
 echo $BIN
