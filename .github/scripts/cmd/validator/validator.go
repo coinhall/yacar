77272e19b15ee6d7b48003d25465b1a4ef70da07
Yusuf Bin Musa <yusuf.musa3@gmail.com>
2024-07-06 13:57:21 +0800

    chore: update CI

--- .github/scripts/cmd/validator/validator.go
+++ .github/scripts/cmd/validator/validator.go
@@ -1,15 +1,16 @@
 package validator
 
 import (
-	"cmp"
 	"log"
 	"path/filepath"
+	"slices"
 	"strings"
 
 	"github.com/coinhall/yacar/internal/unmarshaler"
 	"github.com/coinhall/yacar/internal/walker"
 	"github.com/coinhall/yacar/internal/yacar"
 	"github.com/coinhall/yacarsdk/v2"
+	"golang.org/x/exp/maps"
 )
 
 func Start(filePaths []string, ignoreErrors map[string]struct{}) {
@@ -17,68 +18,55 @@ func Start(filePaths []string, ignoreErrors map[string]struct{}) {
 	log.Println("Validated JSONs successfully...")
 }
 
-type resources struct {
-	account, asset, binary, contract, entity, pool string
-}
-
-func validateYacarJSONs(filePaths []string, ignoreErrors map[string]struct{}) {
-	var err error
+func validateYacarJSONs(filePaths []string, ignore map[string]struct{}) {
 	crm := getChainResources(filePaths)
-	for _, res := range crm {
-		path := cmp.Or(res.account, res.asset, res.binary, res.contract, res.entity, res.pool)
-		switch yacar.MustParse(walker.GetFileNameNoSuffix(path, yacar.FileSuffix)) {
-		case yacar.Account:
-			err = handleAccount(res.account)
-		case yacar.Asset:
-			err = handleAsset(res.asset, res.entity)
-		case yacar.Binary:
-			err = handleBinary(res.binary)
-		case yacar.Contract:
-			err = handleContract(res.contract)
-		case yacar.Entity:
-			err = handleEntity(res)
-		case yacar.Pool:
-			err = handlePool(res.pool)
-		default:
-			panic("unhandled case")
+	keys := maps.Keys(crm)
+	slices.Sort(keys)
+	for _, k := range keys {
+		var (
+			asset, entity, pool, contract, binary, account string
+		)
+		for _, fp := range crm[k] {
+			switch yacar.MustParse(walker.GetFileNameNoSuffix(fp, yacar.FileSuffix)) {
+			case yacar.Asset:
+				asset = fp
+			case yacar.Entity:
+				entity = fp
+			case yacar.Pool:
+				pool = fp
+			case yacar.Contract:
+				contract = fp
+			case yacar.Binary:
+				binary = fp
+			case yacar.Account:
+				account = fp
+			default:
+				panic("unhandled case")
+			}
 		}
 
-		if err == nil {
-			continue
-		}
-
-		if _, ok := ignoreErrors[err.Error()]; !ok {
-			log.Fatalln(err)
-		}
+		errorCheck(handleAccount(account), ignore)
+		errorCheck(handlePool(pool), ignore)
+		errorCheck(handleContract(contract), ignore)
+		errorCheck(handleBinary(binary), ignore)
+		errorCheck(handleAsset(asset, entity), ignore)
+		errorCheck(handleEntity(entity, account, asset, binary,
+			contract, pool), ignore)
 	}
 }
 
-func getChainResources(filePaths []string) map[string]*resources {
-	crm := map[string]*resources{}
+func getChainResources(filePaths []string) map[string][]string {
+	crm := map[string][]string{}
 	for _, fp := range filePaths {
 		fp = filepath.ToSlash(fp)
 		split := strings.Split(filepath.Dir(fp), "/")
 		chain := split[len(split)-1]
 		r, ok := crm[chain]
 		if !ok {
-			r = &resources{}
+			r = make([]string, 0)
 		}
-		switch yacar.MustParse(walker.GetFileNameNoSuffix(fp, yacar.FileSuffix)) {
-		case yacar.Account:
-			r.account = fp
-		case yacar.Asset:
-			r.asset = fp
-		case yacar.Binary:
-			r.binary = fp
-		case yacar.Contract:
-			r.contract = fp
-		case yacar.Entity:
-			r.entity = fp
-		case yacar.Pool:
-			r.pool = fp
-		default:
-			panic("unhandled case")
-		}
+
+		r = append(r, fp)
 		crm[chain] = r
 	}
 	return crm
@@ -137,15 +125,15 @@ func handleContract(fp string) error {
 	return err
 }
 
-func handleEntity(res *resources) error {
-	entities, err := unmarshaler.UnmarshalInto(res.entity, make([]yacarsdk.Entity, 0))
+func handleEntity(entity, account, asset, binary, contract, pool string) error {
+	entities, err := unmarshaler.UnmarshalInto(entity, make([]yacarsdk.Entity, 0))
 	if err != nil {
 		return err
 	}
 	usedEntities := map[string]struct{}{}
 
-	if len(res.account) > 0 {
-		accounts, err := unmarshaler.UnmarshalInto(res.account, make([]yacarsdk.Account, 0))
+	if len(account) > 0 {
+		accounts, err := unmarshaler.UnmarshalInto(account, make([]yacarsdk.Account, 0))
 		if err != nil {
 			return err
 		}
@@ -154,8 +142,8 @@ func handleEntity(res *resources) error {
 		}
 	}
 
-	if len(res.asset) > 0 {
-		assets, err := unmarshaler.UnmarshalInto(res.asset, make([]yacarsdk.Asset, 0))
+	if len(asset) > 0 {
+		assets, err := unmarshaler.UnmarshalInto(asset, make([]yacarsdk.Asset, 0))
 		if err != nil {
 			return err
 		}
@@ -164,8 +152,8 @@ func handleEntity(res *resources) error {
 		}
 	}
 
-	if len(res.binary) > 0 {
-		binaries, err := unmarshaler.UnmarshalInto(res.binary, make([]yacarsdk.Binary, 0))
+	if len(binary) > 0 {
+		binaries, err := unmarshaler.UnmarshalInto(binary, make([]yacarsdk.Binary, 0))
 		if err != nil {
 			return err
 		}
@@ -174,8 +162,8 @@ func handleEntity(res *resources) error {
 		}
 	}
 
-	if len(res.contract) > 0 {
-		contracts, err := unmarshaler.UnmarshalInto(res.contract, make([]yacarsdk.Contract, 0))
+	if len(contract) > 0 {
+		contracts, err := unmarshaler.UnmarshalInto(contract, make([]yacarsdk.Contract, 0))
 		if err != nil {
 			return err
 		}
@@ -196,4 +184,14 @@ func handlePool(fp string) error {
 
 	_, err = yacarsdk.ValidatePools(pools)
 	return err
+}
+
+func errorCheck(err error, ignore map[string]struct{}) {
+	if err == nil {
+		return
+	}
+
+	if _, ok := ignore[err.Error()]; !ok {
+		log.Fatalln(err)
+	}
 }
--- .github/scripts/internal/unmarshaler/unmarshaler.go
+++ .github/scripts/internal/unmarshaler/unmarshaler.go
@@ -2,17 +2,16 @@ package unmarshaler
 
 import (
 	"encoding/json"
+	"errors"
 	"os"
 )
 
 func UnmarshalInto[T any](path string, container T) (T, error) {
-	if _, err := os.Stat(path); os.IsNotExist(err) {
-		return container, err
-	}
-
 	data, err := os.ReadFile(path)
-	if err != nil {
-		return container, err
+	if errors.Is(err, os.ErrNotExist) {
+		return container, nil
+	} else if err != nil {
+		panic(err)
 	}
 
 	if err := json.Unmarshal(data, &container); err != nil {
